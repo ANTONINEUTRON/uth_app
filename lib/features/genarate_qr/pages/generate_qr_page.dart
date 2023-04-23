7182bf4086873b5304873bd5869001a06643797f
AntoniNeutron <antonineutron@gmail.com>
2023-04-23 15:25:36 +0100

    added firebase and tweaKED UI

--- lib/features/genarate_qr/pages/generate_qr_page.dart
+++ lib/features/genarate_qr/pages/generate_qr_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 class GenerateQRPage extends StatefulWidget {
   const GenerateQRPage({Key? key}) : super(key: key);
@@ -9,8 +10,8 @@ class GenerateQRPage extends StatefulWidget {
 
 class _GenerateQRPageState extends State<GenerateQRPage> {
   String? drugName;
-  String? expiryDate;
-  String? manufacturingDate;
+  DateTime expiryDate = DateTime.now();
+  DateTime manufacturingDate = DateTime.now();
   String? batchNumber;
   String? dosage;
   String? storingCondition;
@@ -36,26 +37,26 @@ class _GenerateQRPageState extends State<GenerateQRPage> {
             ),
           ),
           const SizedBox(height: 5,),
-          TextField(
-            onChanged: (value)=>setState(() {
-              expiryDate = value;
-            }),
-            decoration: InputDecoration(
-              border: OutlineInputBorder(),
-              labelText: "Expiry Date"
-            ),
-          ),
-          const SizedBox(height: 5,),
-          TextField(
-            onChanged: (value)=>setState(() {
-              manufacturingDate = value;
-            }),
-            decoration: InputDecoration(
-              border: OutlineInputBorder(),
-              labelText: "Manufacture Date"
-            ),
-          ),
-          const SizedBox(height: 5,),
+          // TextField(
+          //   onChanged: (value)=>setState(() {
+          //     expiryDate = value;
+          //   }),
+          //   decoration: InputDecoration(
+          //     border: OutlineInputBorder(),
+          //     labelText: "Expiry Date"
+          //   ),
+          // ),
+          // const SizedBox(height: 5,),
+          // TextField(
+          //   onChanged: (value)=>setState(() {
+          //     manufacturingDate = value;
+          //   }),
+          //   decoration: InputDecoration(
+          //     border: OutlineInputBorder(),
+          //     labelText: "Manufacture Date"
+          //   ),
+          // ),
+          // const SizedBox(height: 5,),
           TextField(
             onChanged: (value)=>setState(() {
               batchNumber = value;
@@ -86,6 +87,68 @@ class _GenerateQRPageState extends State<GenerateQRPage> {
             ),
           ),
           const SizedBox(height: 10,),
+          Padding(
+              padding: EdgeInsets.all(8),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  MaterialButton(
+                    color: Colors.white70,
+                    onPressed: (){
+                      showDatePicker(
+                          context: context,
+                          initialDate: DateTime.now(),
+                          firstDate: DateTime.now(),
+                          lastDate: DateTime(2100)
+                      ).then((value){
+                        setState((){
+                          manufacturingDate = value ?? DateTime.now();
+                        });
+                      });
+                    },
+                    child: Row(
+                      children: [
+                        Text(
+                            manufacturingDate.isBefore(DateTime.now())
+                                ? "Manufacture Date"
+                                : DateFormat('E, MMM dd y').format(manufacturingDate)
+                        ),
+                        manufacturingDate.isBefore(DateTime.now())
+                            ? Icon(Icons.calendar_today)
+                            : Icon(Icons.edit)
+                      ],
+                    ),
+                  ),
+                  MaterialButton(
+                    color: Colors.white70,
+                    onPressed: (){
+                      showDatePicker(
+                          context: context,
+                          initialDate: DateTime.now(),
+                          firstDate: DateTime.now(),
+                          lastDate: DateTime(2100)
+                      ).then((value){
+                        setState((){
+                          expiryDate = value ?? DateTime.now();
+                        });
+                      });
+                    },
+                    child: Row(
+                      children: [
+                        Text(
+                            expiryDate.isBefore(DateTime.now())
+                                ? "Expiry Date"
+                                : DateFormat('E, MMM dd y').format(expiryDate)
+                        ),
+                        expiryDate.isBefore(DateTime.now())
+                            ? Icon(Icons.calendar_today)
+                            : Icon(Icons.edit)
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+          ),
           ElevatedButton(
               onPressed: (){
                 //generate and save qr code
---
+++ lib/service/firebase_service.dart
@@ -0,0 +1,5 @@
+
+//This class serve as an interface with interacting with firestore
+class FirestoreService{
+
+}
